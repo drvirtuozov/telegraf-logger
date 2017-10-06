d6ff43676fb62e3ac4fc4b64aea2190f0ee6aac6
drvirtuozov <drvirtuozov@ya.ru>
2017-10-06 23:53:56 +0300

    Add new placeholders

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ class TelegrafLogger {
   constructor(options) {
     this.options = Object.assign({
       log: console.log,
-      format: '%botUsername => *%sceneId* [%username] %firstName %lastName (%id): <%updateType> %content',
+      format: '@%username %firstName %lastName (%fromId): <%updateType> %content',
       contentLength: 100,
     }, options);
   }
@@ -34,18 +34,30 @@ class TelegrafLogger {
           content = ctx.inlineQuery.query;
           break;
 
+        case 'shipping_query':
+          content = ctx.shippingQuery.invoice_payload;
+          break;
+
+        case 'pre_checkout_query':
+          content = ctx.preCheckoutQuery.invoice_payload;
+          break;
+
         default: content = '';
       }
 
       const text = this.options.format
         .replace(/%botUsername/g, ctx.me || null)
-        .replace(/%sceneId/g, (ctx.session && ctx.session._flow && ctx.session._flow.id) || null)
         .replace(/%username/g, ctx.from.username || null)
         .replace(/%firstName/g, ctx.from.first_name)
         .replace(/%lastName/g, ctx.from.last_name || '')
-        .replace(/%id/g, ctx.from.id)
-        .replace(/%updateType/g, ctx.updateSubType || ctx.updateSubTypes[0] || ctx.updateType)
+        .replace(/%fromId/g, ctx.from.id)
+        .replace(/%chatId/g, ctx.chat.id)
+        .replace(/%messageId/g, ctx.message.message_id)
+        .replace(/%updateType/g, ctx.updateType)
+        .replace(/%updateSubType/g, ctx.updateSubType || ctx.updateSubTypes[0] || ctx.updateType)
+        .replace(/%sceneId/g, (ctx.session && ctx.session._flow && ctx.session._flow.id) || null)
         .replace(/ +|\n/g, ' ');
+
       content = content.replace(/\n/g, ' ');
 
       if (content.length > this.options.contentLength) {
