de59396f01f295316da20443b8112f9f90242425
drvirtuozov <drvirtuozov@ya.ru>
2017-10-07 06:48:20 +0300

    Add updateTypeId placeholder

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ class TelegrafLogger {
   middleware() {
     return (ctx, next) => {
       let content;
+      let updateTypeId;
 
       switch (ctx.updateType) {
         case 'message':
+          updateTypeId = ctx.message.message_id;
+
           if (ctx.message.text) {
             content = ctx.message.text;
           } else if (ctx.message.sticker) {
@@ -27,49 +30,57 @@ class TelegrafLogger {
           break;
 
         case 'edited_message':
+          updateTypeId = ctx.editedMessage.message_id;
           content = ctx.editedMessage.text;
           break;
 
         case 'callback_query':
+          updateTypeId = ctx.callbackQuery.id;
           content = ctx.callbackQuery.data;
           break;
 
         case 'inline_query':
+          updateTypeId = ctx.inlineQuery.id;
           content = ctx.inlineQuery.query;
           break;
 
         case 'shipping_query':
+          updateTypeId = ctx.shippingQuery.id;
           content = ctx.shippingQuery.invoice_payload;
           break;
 
         case 'pre_checkout_query':
+          updateTypeId = ctx.preCheckoutQuery.id;
           content = ctx.preCheckoutQuery.invoice_payload;
           break;
 
-        default: content = '';
+        default:
+          content = '';
+          updateTypeId = 0;
       }
 
       const text = this.options.format
-        .replace(/%botUsername/igm, ctx.me || null)
-        .replace(/%username/igm, ctx.from.username || null)
-        .replace(/%firstName/igm, ctx.from.first_name)
-        .replace(/%lastName/igm, ctx.from.last_name || '')
-        .replace(/%fromId/igm, ctx.from.id)
-        .replace(/%chatId/igm, ctx.chat && ctx.chat.id)
-        .replace(/%chatType/igm, ctx.chat && ctx.chat.type)
-        .replace(/%chatTitle/igm, (ctx.chat && ctx.chat.title) || null)
-        .replace(/%chatUsername/igm, (ctx.chat && ctx.chat.username) || null)
-        .replace(/%updateId/igm, ctx.update.update_id)
-        .replace(/%updateType/igm, ctx.updateType)
-        .replace(/%updateSubType/igm, ctx.updateSubType || ctx.updateSubTypes[0] || ctx.updateType)
-        .replace(/%sceneId/igm, (ctx.session && ctx.session._flow && ctx.session._flow.id) || null)
+        .replace(/%botUsername\b/igm, ctx.me || null)
+        .replace(/%username\b/igm, ctx.from.username || null)
+        .replace(/%firstName\b/igm, ctx.from.first_name)
+        .replace(/%lastName\b/igm, ctx.from.last_name || '')
+        .replace(/%fromId\b/igm, ctx.from.id)
+        .replace(/%chatId\b/igm, ctx.chat && ctx.chat.id)
+        .replace(/%chatType\b/igm, ctx.chat && ctx.chat.type)
+        .replace(/%chatTitle\b/igm, (ctx.chat && ctx.chat.title) || null)
+        .replace(/%chatUsername\b/igm, (ctx.chat && ctx.chat.username) || null)
+        .replace(/%updateId\b/igm, ctx.update.update_id)
+        .replace(/%updateType\b/igm, ctx.updateType)
+        .replace(/%updateTypeId\b/igm, updateTypeId)
+        .replace(/%updateSubType\b/igm, ctx.updateSubType || ctx.updateSubTypes[0] || ctx.updateType)
+        .replace(/%sceneId\b/igm, (ctx.session && ctx.session._flow && ctx.session._flow.id) || null)
         .replace(/ +|\n/g, ' ');
 
       if (content.length > this.options.contentLength) {
         content = `${content.slice(0, this.options.contentLength)}...`;
       }
 
-      this.options.log(text.replace(/%content/igm, content).replace(/\n/g, ' '));
+      this.options.log(text.replace(/%content\b/igm, content).replace(/\n/g, ' '));
       return next();
     };
   }
